c12d21c65249b5c0fe347338832506675b7bf1d4
Yuto-S27 <g1827043@st.t-kougei.ac.jp>
2020-05-23 15:34:27 +0900

    no message

---
+++ src/UnitTest1/UnitTest1.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "PrimeNumberLib.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			Assert::AreEqual(is_prime_number(1), false);
+			Assert::AreEqual(is_prime_number(7), true); 
+			Assert::AreEqual(is_prime_number(15), false);
+		}
+	};
+}
